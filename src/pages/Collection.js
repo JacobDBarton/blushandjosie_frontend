58a981501ba4bbd34c1b4d2cac00dfe713a37407
Jacob Barton <jacobdbarton3@gmail.com>
2022-06-06 16:40:04 -0400

    carousel works and minor tweaks

--- src/pages/Collection.js
+++ src/pages/Collection.js
@@ -1,30 +1,12 @@
-import { useState, useEffect } from "react";
+import React from 'react'
 
-function Collection(props) {
 
-  const [earrings, setEarrings] = useState(null);
-
-  const getEarringData = async () => {
-
-    const response = await fetch(props.URL + "earrings");
-
-    const data = await response.json();
-
-    setEarrings(data);
-  };
-
-  useEffect(() => getEarringData(), []);
-
-  const loaded = () => {
-    return earrings.map((earrings) => (
-      <div>
-        <h1>{earrings.name}</h1>
-        <img src={earrings.image} alt={earrings} />
-      </div>
-    ));
-  };
-
-  return earrings ? loaded() : <h1>Loading earrings</h1>;
+function Collection() {
+  return (
+    <div> 
+ 
+    </div>
+  )
 }
 
-export default Collection;
+export default Collection
--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,28 +1,12 @@
-import { useState, useEffect } from "react";
+import React from 'react'
 
-function Earring(props) {
-  const [show, setShow] = useState(null);
 
-  const getShowData = async () => {
-
-    const response = await fetch(props.URL + "show");
-
-    const data = await response.json();
-
-    setShow(data);
-  };
-
-  useEffect(() => getShowData(), []);
-
-  const loaded = () => (
-    <div>
-      <h2>{show.name}</h2>
-      <h3>{show.price}</h3>
-      <p>{show.description}</p>
+function Show() {
+  return (
+    <div> 
+ 
     </div>
-  );
-
-  return show ? loaded() : <h1>Loading...</h1>;
+  )
 }
 
-export default Earring;
+export default Show
