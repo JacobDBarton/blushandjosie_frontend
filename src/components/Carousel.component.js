9dad0f39bb1f9ffdc5c6862fe0ff28ba5386a8ac
Jacob Barton <jacobdbarton3@gmail.com>
2022-06-09 10:00:42 -0400

    trying routes before controllers

--- src/components/Carousel.component.js
+++ src/components/Carousel.component.js
@@ -5,7 +5,7 @@ import "react-responsive-carousel/lib/styles/carousel.min.css";
 export default function CarouselComponent() {
     return (
         <div className="carousel-wrapper">
-            <Carousel interval={3000}>
+            <Carousel interval={3000} showThumbs={false}>
                 <div>
                     <img src="../images/blush.webp" alt=""/>
                 </div>
